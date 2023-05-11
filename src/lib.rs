34746c84bed687b9786c18385660b7903507872c
Iwancof <rock.iwanof@gmail.com>
2023-05-11 21:41:18 +0900

    add Debug attribute

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,52 @@
 #[deny(unsafe_code)]
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{self, parse_macro_input};
+use syn::{self, parse::Parse, parse::ParseStream, parse_macro_input, Ident, Result};
+
+#[derive(Debug)]
+struct LayoutRandomizeArgs {
+    debug: Option<Ident>,
+}
+
+impl Parse for LayoutRandomizeArgs {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let debug = if input.peek(syn::Ident) { // TODO: parse multiple args
+            Some(input.parse()?)
+        } else {
+            None
+        };
+        Ok(LayoutRandomizeArgs { debug })
+    }
+}
 
 #[proc_macro_attribute]
-pub fn layout_randomize(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn layout_randomize(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(args as LayoutRandomizeArgs);
     let mut input = parse_macro_input!(input as syn::ItemStruct);
 
+    let struct_name = &input.ident;
+
     let punctuated = if let syn::Fields::Named(ref mut name) = input.fields {
+        // punctuated has field name.
         &mut name.named
-    } else if let syn::Fields::Unnamed(ref mut unnamed) = input.fields {
-        &mut unnamed.unnamed
     } else {
-        return (quote! { #input }).into();
+        panic!("layout_randomize only supports named struct");
     };
 
     let mut vector: Vec<syn::Field> = Vec::new();
     while let Some(pair) = punctuated.pop() {
         vector.push(pair.into_tuple().0);
+    }
+
+
+    let mut fields = quote! {};
+    for v in vector.iter() {
+        let id = v.ident.clone().unwrap(); // since punctuated has fields name, this unwrap will
+                                           // never panic.
+        fields = quote! {
+            .field(stringify!(#id), &self.#id)
+            #fields
+        };
     }
 
     use rand::{rngs::OsRng, seq::SliceRandom};
@@ -27,9 +56,29 @@ pub fn layout_randomize(_args: TokenStream, input: TokenStream) -> TokenStream {
         punctuated.push(v);
     }
 
-    return (quote! {
+    let debug_impl = if let Some(debug_ident) = args.debug {
+        if debug_ident == "Debug" {
+            Some(quote! {
+                impl std::fmt::Debug for #struct_name {
+                    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        f.debug_struct(stringify!(#struct_name))
+                            #fields
+                            .finish()
+                    }
+                }
+            })
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    let output = quote! {
         #[repr(C)] // TODO: add to input.attrs
         #input
-    })
-    .into();
+        #debug_impl
+    };
+
+    output.into()
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -3,7 +3,50 @@ extern crate random_struct_layout;
 
 #[cfg(test)]
 mod tests {
+    #[allow(unused)]
     use super::*;
+
+    #[test]
+    fn randomize_test() {
+        use offset;
+
+        struct DataStruct {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+            e: i32,
+            f: i32,
+            g: i32,
+        }
+
+        assert!((offset::offset_of!(DataStruct::a) == 0 &&
+                  offset::offset_of!(DataStruct::b) == 4 &&
+                  offset::offset_of!(DataStruct::c) == 8 &&
+                  offset::offset_of!(DataStruct::d) == 12 &&
+                  offset::offset_of!(DataStruct::e) == 16 &&
+                  offset::offset_of!(DataStruct::f) == 20 &&
+                  offset::offset_of!(DataStruct::g) == 24));
+
+        #[layout_randomize]
+        struct RandomizedDataStruct {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+            e: i32,
+            f: i32,
+            g: i32,
+        }
+
+        assert!(!(offset::offset_of!(RandomizedDataStruct::a) == 0 &&
+                  offset::offset_of!(RandomizedDataStruct::b) == 4 &&
+                  offset::offset_of!(RandomizedDataStruct::c) == 8 &&
+                  offset::offset_of!(RandomizedDataStruct::d) == 12 &&
+                  offset::offset_of!(RandomizedDataStruct::e) == 16 &&
+                  offset::offset_of!(RandomizedDataStruct::f) == 20 &&
+                  offset::offset_of!(RandomizedDataStruct::g) == 24));
+    }
 
     #[test]
     fn normal_access() {
@@ -13,7 +56,7 @@ mod tests {
             b: u32,
             c: u128,
             s: String,
-        };
+        }
 
         let d = Data {
             a: 42,
@@ -36,7 +79,63 @@ mod tests {
             a: i32,
             b: u32,
             c: u128,
+        }
+    }
+
+    #[test]
+    fn debug_output() {
+        #[layout_randomize(Debug)]
+        struct Randomized {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+            e: i32,
+            f: i32,
+            g: i32,
+        }
+
+        #[derive(Debug)]
+        #[allow(unused)]
+        struct NotRandomized {
+            a: i32,
+            b: i32,
+            c: i32,
+            d: i32,
+            e: i32,
+            f: i32,
+            g: i32,
+        }
+
+        let randomized = Randomized {
+            a: 1,
+            b: 2,
+            c: 3,
+            d: 4,
+            e: 5,
+            f: 6,
+            g: 7,
         };
+        let not_randomized = NotRandomized {
+            a: 1,
+            b: 2,
+            c: 3,
+            d: 4,
+            e: 5,
+            f: 6,
+            g: 7,
+        };
+
+        let debug_randomized = format!("{:?}", randomized).replace("Randomized", "");
+        let debug_not_randomized = format!("{:?}", not_randomized).replace("NotRandomized", "");
+
+        assert_eq!(debug_randomized, debug_not_randomized);
+    }
+
+    #[test]
+    fn empty_debug() {
+        #[layout_randomize(Debug)]
+        struct Empty {}
     }
 
     // any other tests?
